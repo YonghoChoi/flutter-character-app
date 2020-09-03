adb6d72370ee02941cc40fa695acfa6d8037f96d
YonghoChoi <yongho1037@gmail.com>
2020-09-04 00:53:50 +0900

    카카오 라이언 그림 및 설명글 추가

--- character_app/lib/main.dart
+++ character_app/lib/main.dart
@@ -7,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BBANTO',
+      debugShowCheckedModeBanner: false,  // 에뮬레이터의 debug 표시를 없앰
+      title: 'KAKAO',
       home: Grade(),
     );
   }
@@ -17,10 +18,14 @@ class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
+      backgroundColor: Colors.yellowAccent,
       appBar: AppBar(
-        title: Text('BBANTO'),
-        backgroundColor: Colors.amber[700],
+        title: Text('KAKAO',
+            style: TextStyle(
+            color: Colors.white,
+        ),
+        ),
+        backgroundColor: Colors.cyan,
         centerTitle: true,
         elevation: 0.0,
       ),
@@ -29,20 +34,101 @@ class Grade extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('NAME',
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/lion.gif'),
+                radius: 60.0,
+                backgroundColor: Colors.yellowAccent
+              ),
+            ),
+            Divider(
+              height: 60.0, // divider 선을 기준으로 위 아래 여백의 합을 의미
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text('이름',
             style: TextStyle(
-              color: Colors.white,
+              color: Colors.black,
               letterSpacing: 2.0
             )),
             SizedBox(
               height: 10.0,
             ),
-            Text('BBANTO',
+            Text('라이언',
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
                 letterSpacing: 2.0,
                 fontSize: 28.0,
                 fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text('성격',
+                style: TextStyle(
+                    color: Colors.black,
+                    letterSpacing: 2.0
+                )),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('발랄',
+              style: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(  // 위젯을 가로로 나열
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('귀여움',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                )),
+              ],
+            ),
+            Row(  // 위젯을 가로로 나열
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('친근함',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    )),
+              ],
+            ),
+            Row(  // 위젯을 가로로 나열
+              children: [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text('인기많음',
+                    style: TextStyle(
+                        fontSize: 16.0,
+                        letterSpacing: 1.0
+                    )),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/lion.gif'),
+                radius: 40.0,
+                backgroundColor: Colors.yellowAccent,
               ),
             ),
           ],
